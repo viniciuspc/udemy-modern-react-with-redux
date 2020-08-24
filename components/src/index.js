d419b62c1584b8307c2af09b0194a06b944874b8
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-24 12:07:10 +0100

    36. Passing Multiple Props - Practice

--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail'
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM"/>
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM"/>
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM"/>
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" comment="Interesting!" imgSrc={faker.image.avatar()}/>
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" comment="I enjoyed this blog post." imgSrc={faker.image.avatar()}/>
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" comment="Well done" imgSrc={faker.image.avatar()}/>
     </div>
   );
 };
