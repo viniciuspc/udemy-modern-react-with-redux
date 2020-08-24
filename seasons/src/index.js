3d0f3d3327ce851fcadcfecf56aa4d41becfc8f0
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-24 18:05:04 +0100

    50. Getting a Users Physical Location

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReacDOM from "react-dom";
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
   return <div>Hi there!</div>;
 };
 
