419ed70f6ed2cf4add21710b478177bdda023259
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-25 18:05:56 +0100

    88. Solving Context Issues

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,10 +2,10 @@ import React from "react";
 
 class SearchBar extends React.Component {
   state = { term: "" };
-  onFormSubmit(event) {
+  onFormSubmit = (event) => {
     event.preventDefault();
     console.log(this.state.term);
-  }
+  };
   render() {
     return (
       <div className="ui segment">
