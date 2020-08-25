235caf498ee43211102993d3b9b3784a9243747d
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-25 11:24:28 +0100

    63. Alternate State Initialization

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,22 +2,7 @@ import React from "react";
 import ReacDOM from "react-dom";
 
 class App extends React.Component {
-  //(JavaScript specific) Optional.
-  // Is the very first function which will
-  // be caled when an instance of this object is created.
-  constructor(props) {
-    // Call React.Component constructor, which has some
-    // amout of setup required for the component to work
-    // properly.
-    // It is a refence to the parent's constructor.
-    super(props);
-
-    // When we do not know the value of a property
-    // we default it to null.
-    // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
-    // TO this.state.
-    this.state = { lat: null, errorMessage: "" };
-  }
+  state = { lat: null, errorMessage: "" };
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
