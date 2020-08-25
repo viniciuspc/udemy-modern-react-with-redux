7ac529caf8d8b52ae0164c63cede8bb754892b42
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-25 16:59:31 +0100

    81. Adding a Touch of Style

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import SearchBar from './SearchBar'
+import React from "react";
+import SearchBar from "./SearchBar";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container" style={{ marginTop: "10px" }}>
       <SearchBar />
     </div>
   );
-}
+};
 
 export default App;
