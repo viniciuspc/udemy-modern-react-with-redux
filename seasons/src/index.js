660723695e63d22b262e5b483e393211139b97e5
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-25 09:39:46 +0100

    56. Updating State Properties

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,22 +2,34 @@ import React from "react";
 import ReacDOM from "react-dom";
 
 class App extends React.Component {
-  //(JavaScript specific) Optional. Is the very first function which will be caled when an instance of this object is created.
+  //(JavaScript specific) Optional.
+  // Is the very first function which will
+  // be caled when an instance of this object is created.
   constructor(props) {
-    //Call React.Component constructor, which has some amout of setup required for the component to work properly.
-    //It is a refence to the parent's constructor.
+    // Call React.Component constructor, which has some
+    // amout of setup required for the component to work
+    // properly.
+    // It is a refence to the parent's constructor.
     super(props);
-    //When we do not know the value of a property we default it to null
+
+    // When we do not know the value of a property
+    // we default it to null.
+    // THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT
+    // TO this.state.
     this.state = { lat: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //To update or state we called setState!!
+        //The setState method cames from React.Component
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
   }
   // (React specific) React says we have to define render !!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
-    return <div>Latitude: </div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
