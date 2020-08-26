432e9aa66751655d8de6850e2f7893a870d2de3c
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-26 11:38:50 +0100

    99. Implementing Keys in Lists

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -6,12 +6,9 @@ import ImageList from "./ImageList";
 class App extends React.Component {
   state = { images: [] };
   onSearchSubmit = async (term) => {
-    const response = await unsplash.get(
-      "https://api.unsplash.com/search/photos",
-      {
-        params: { query: term },
-      }
-    );
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term },
+    });
 
     this.setState({ images: response.data.results });
   };
--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => {
-    return <img src={image.urls.regular} />;
+  const images = props.images.map(({ description, id, urls }) => {
+    //Include the key property to the root node returned by the map function.
+    return <img alt={description} key={id} src={urls.regular} />;
   });
 
   return <div>{images}</div>;
