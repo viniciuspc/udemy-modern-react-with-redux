c891cd3def6b63883d8d69d3ddbeb4f0df08275d
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-28 17:41:42 +0100

    124. Deeply Nested Callbacks

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,7 +4,7 @@ import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
@@ -15,11 +15,19 @@ class App extends React.Component {
       videos: response.data.items,
     });
   };
+
+  onVideoSelect = (video) => {
+    console.log("From the App!", video);
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
