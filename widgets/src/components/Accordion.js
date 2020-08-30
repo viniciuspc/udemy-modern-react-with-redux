1b8c64868a59b9cbc346ecbf6389bab51c12fb67
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-30 10:15:59 +0100

    134. Helper Functions in Function Components

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const onTitleClick = (index) => {
+    console.log("Title clicked", index);
+  };
+  const renderedItems = items.map((item, index) => {
     return (
       <React.Fragment key={item.title}>
-        <div className="title active">
+        <div
+          className="title active"
+          //Wrap with arrow function the onTitleClick is only executed when the div is clicked
+          //Without the arrow function the onTitleClick will be executed as soon as it receives the parameter
+          onClick={() => onTitleClick(index)}
+        >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
