aacdbec535d567e1193dfb7d14ef43d12417110c
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-31 14:41:30 +0100

    149. Executing the Request from useEffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
@@ -9,8 +10,20 @@ const Search = () => {
   //Array with elements: Run at initial render and after every rerender,
   //                     if one of the elements has changed
   useEffect(() => {
-    console.log("abcde")
-  });
+    const search = async () => {
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term
+        }
+      });
+    };
+
+    search();
+  }, [term]);
 
   return (
     <div>
