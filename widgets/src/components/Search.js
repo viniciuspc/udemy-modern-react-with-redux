9c0254ab14b1a9bfde790249a4a5008f4b815264
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-08-31 14:15:42 +0100

    146. The useEffect Hook

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+
+  //The second argument of the useEffect can be
+  //Empty array: Rua at initial render.
+  //Nothing: Rua at initial render and after every rerender.
+  //Array with elements: Run at initial render and after every rerender,
+  //                     if one of the elements has changed
+  useEffect(() => {
+    console.log("abcde")
+  });
+
   return (
     <div>
       <div className="ui form">
