b5a6af8bb73771728cb4b6c780ca68a9006dd84d
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-08 17:08:02 +0100

    245. Shortened Syntax with Redux Thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,11 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
-export const fetchPosts = async () => {
-  const response = await jsonPlaceholder.get("/post");
-  console.log("teste");
-  return {
-    type: "FETCH_POSTS",
-    payload: response,
-  };
+
+// React Thunk will call our function, passing dispatch and getStae as arguments
+// With dispatch we can write any data we want at redux state store
+// With getSteate we can read any data we want from redux state store
+// Then we need to manualy dispatch an action
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
+
+  dispatch({ type: "FETCH_POSTS", payload: response });
 };
