9caa8f6753eddb796c36357fbb7887352311ff46
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-08 16:13:32 +0100

    240. Making a Request From an Action Creator

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,9 @@
-export const fetchPosts = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+export const fetchPosts = async () => {
+  const response = await jsonPlaceholder.get("/post");
+  console.log("teste");
   return {
     type: "FETCH_POSTS",
+    payload: response,
   };
 };
