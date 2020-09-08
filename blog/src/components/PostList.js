7c16429a7ecd41eace231db4c3f9fd3fb170fa7d
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-08 16:01:01 +0100

    239. Wiring Up an Action Creator

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
   render() {
@@ -6,4 +8,4 @@ class PostList extends React.Component {
   }
 }
 
-export default PostList;
+export default connect(null, { fetchPosts })(PostList);
