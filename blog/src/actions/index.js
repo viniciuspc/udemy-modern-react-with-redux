304c779050946aa77251c3560bec6a8b18946d56
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-09 18:03:22 +0100

    264. Memoization Issues

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 // React Thunk will call our function, passing dispatch and getStae as arguments
@@ -10,8 +11,16 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
+// export const fetchUser = (id) => async (dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// };
 
-  dispatch({ type: "FETCH_USER", payload: response.data });
+export const fetchUser = function (id) {
+  return _.memoize(async function (dispatch) {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: "FETCH_USER", payload: response.data });
+  });
 };
