0abf70046a5fd0ae424607615777d1c3f29cad97
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2020-09-09 18:10:28 +0100

    265. One Time Memoization

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -11,16 +11,13 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-// export const fetchUser = (id) => async (dispatch) => {
-//   const response = await jsonPlaceholder.get(`/users/${id}`);
-//
-//   dispatch({ type: "FETCH_USER", payload: response.data });
-// };
+//A function that returns a function that calls _fetchUser(id, dispatch)
+export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
 
-export const fetchUser = function (id) {
-  return _.memoize(async function (dispatch) {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
+// Uderscore Indicates a private function.
+// This function is the memoize function
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
 
-    dispatch({ type: "FETCH_USER", payload: response.data });
-  });
-};
+  dispatch({ type: "FETCH_USER", payload: response.data });
+});
